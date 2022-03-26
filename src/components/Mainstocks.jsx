61f0c80ffe64e8c8c04986fe516f998de4866992
therealestdeal <mieyle@protonmail.com>
2022-03-26 18:54:41 +0000

    Add component that renders the closecards

---
+++ src/components/Mainstocks.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Closecard from "./Closecard";
+
+
+function Mainstocks({stockArr}) {
+
+    return (
+        <div>
+                {stockArr.map(el=>{
+                    return( <Closecard key={el} symbol={el}/>)
+                    
+                })}
+      
+            
+        </div>
+    )
+}
+
+export default Mainstocks
