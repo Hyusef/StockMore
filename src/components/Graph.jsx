8ac52a1ed7193f609528833f8abe82e947773d30
therealestdeal <mieyle@protonmail.com>
2022-03-26 13:03:44 +0000

    Configure component to fetch from backend instead of vantage api

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { format, subDays} from "date-fns";
 import {
   Chart as ChartJS,
   LineElement,
@@ -9,9 +11,8 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
-import { useQuery } from "react-query";
 import axios from "axios";
-import CircularProgress from "@mui/material/CircularProgress";
+
 
 ChartJS.register(
   CategoryScale,
@@ -23,49 +24,34 @@ ChartJS.register(
 );
 
 function Graph() {
-  const { data, error } = useQuery(
-    "stock data",
-    () =>
-      axios(
-        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=${process.env.REACT_APP_STOCK_API_KEY}&outputsize=compact`
-      ),
-    {
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-      staleTime: 1000000,
-      cacheTime: 1000000000,
-    }
-  );
+  const [histData, setHistData] = useState([]);
 
-  console.log(data);
-
-  const banana = [];
-  const rats = [];
-  if (error) {
-    return <h1>Error:{error.message}</h1>;
-  }
-
-  if (data === undefined) {
-    return <CircularProgress />;
-  }
-
-  for (var key in data.data["Time Series (Daily)"]) {
-    banana.push(key);
-    rats.push(data.data["Time Series (Daily)"][key]["1. open"]);
-  }
-
-  banana.splice(7);
-  rats.splice(7);
+  useEffect(() => {
+    const pastWeek = format(subDays(new Date(), 7), "yyyy-MM-dd");
+    const option = ["Ibm", pastWeek];
+    axios.get("/stock", { params: option }).then((res) => {
+      setHistData(res.data);
+    });
+  }, []);
+  
+  const closes = [];
+  const dates = [];
+  histData.forEach(el => {
+    const histDate = el["date"].slice(0, 10);
+    dates.push(histDate);
+    const histClose = +el["adjClose"].toFixed(2);
+    closes.push(histClose);
+  });
 
   return (
     <div>
       <Line
         data={{
-          labels: banana,
+          labels: dates,
           datasets: [
             {
-              label: "Open",
-              data: rats,
+              label: "Close",
+              data: closes,
               fill: false,
               borderColor: "rgb(75, 192, 192)",
               tension: 0.1,
