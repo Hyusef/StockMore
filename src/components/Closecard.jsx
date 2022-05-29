f945021e2b47b68223367080717e2db95df01652
therealestdeal <mieyle@protonmail.com>
2022-05-29 15:08:01 +0100

    Refactor and extract new component

--- src/components/Closecard.jsx
+++ src/components/Closecard.jsx
@@ -1,57 +1,11 @@
 import React from "react";
-import styled from "styled-components";
-import { css } from "styled-components";
-import Card from "@mui/material/Card";
-import { useQuery } from "react-query";
-import { subDays, format, getISODay } from "date-fns";
-import Graph from "./Graph";
-import CircularProgress from "@mui/material/CircularProgress";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-import Button from "@mui/material/Button";
-import { Textfit } from "react-textfit";
-
-const Wrapper = styled.div`
-  padding: 5px;
-  min-width: 10%;
-  button {
-    background: none;
-    color: inherit;
-    border: none;
-    font: inherit;
-    cursor: pointer;
-    outline: inherit;
-    color: darkred;
-    margin-top: auto;
-  }
-`;
-
-const CostWrapper = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const Cost = styled.h4`
-  ${(props) => {
-    if (props.myProps === "green")
-      return `
-        color: #03a811;
-    
-    `;
-    if (props.myProps === "red")
-      return `
-      color:#781202;
-    `;
-  }}
-`;
+import CardWrapper from "./CardWrapper";
 
 function Closecard({ symbol, dHand }) {
   const [quotesData, setQuotesData] = useState([]);
   const [isLoading, setIsLoading] = useState([]);
-  const [showCard, setShowCard] = useState(false);
 
   useEffect(() => {
     axios
@@ -75,50 +29,16 @@ function Closecard({ symbol, dHand }) {
 
   return (
     <div>
-      <Wrapper>
-        <Card
-          onMouseEnter={() => setShowCard(true)}
-          onMouseLeave={() => setShowCard(false)}
-          sx={{
-            bgcolor: "#08081b",
-            color: "#d4d7e6",
-            padding: "15px",
-            borderRadius: "15px;",
-            margin: "10px",
-            width: "250px",
-            height: "350px",
-          }}
-        >
-          {isLoading && <CircularProgress />}
-          {!isLoading && (
-            <>
-              <Textfit mode="multi">
-                {quotesData.symbol} | {quotesData.longName}
-              </Textfit>
-
-              <h4>
-                {quotesData.fullExchangeName} | {quotesData.currency}
-              </h4>
-              {quotesData.region}
-              <CostWrapper>
-                <Cost myProps={`${isBullish ? "green" : "red"}`}>{qts}</Cost>
-                {isBullish && <ArrowUpwardIcon sx={{ color: "#03a811" }} />}
-                {!isBullish && <ArrowDownwardIcon sx={{ color: "#781202" }} />}
-              </CostWrapper>
-              <Graph symbol={symbol} />
-              {showCard && (
-                <Button onClick={() => dHand(quotesData.symbol)}>
-                  <DeleteOutlineIcon />
-                </Button>
-              )}
-            </>
-          )}
-        </Card>
-      </Wrapper>
+      <CardWrapper
+        isLoading={isLoading}
+        isBullish={isBullish}
+        qts={qts}
+        symbol={symbol}
+        dHand={dHand}
+        quotesData={quotesData}
+      />
     </div>
   );
 }
-
-// {fiSuccess?<Cost className="banana">{parseFloat(fiData['Time Series (Daily)'][formattedDate]['1. open']).toFixed(2)}</Cost>:"hello there"}
 
 export default Closecard;
