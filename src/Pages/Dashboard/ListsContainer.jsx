2f4dfeaaf8c5c9d11a90cd3cf3375baa33493195
therealestdeal <mieyle@protonmail.com>
2023-03-04 07:00:23 +0000

    Change company arrays

--- src/Pages/Dashboard/ListsContainer.jsx
+++ src/Pages/Dashboard/ListsContainer.jsx
@@ -8,33 +8,37 @@ width:100vw;
 background:#08081b
 `
 
-
 const TechCompanies =
     [
         {
             'name': 'Apple',
             'sector': 'Technology',
             'country': 'US',
+            'symbol': 'AAPL'
         },
         {
             'name': 'Microsoft',
             'sector': 'Technology',
             'country': 'US',
+            'symbol': 'MSFT'
         },
         {
             'name': 'Amazon',
             'sector': 'Technology',
             'country': 'US',
+            'symbol': 'AMZN'
         },
         {
             'name': 'Ibm',
             'sector': 'Technology',
             'country': 'US',
+            'symbol': 'IBM'
         },
         {
             'name': 'Meta',
             'sector': 'Technology',
             'country': 'US',
+            'symbol': 'META'
         }
     ];
 
@@ -44,55 +48,66 @@ const AutomotiveCompanies =
             'name': 'Toyota',
             'sector': 'Automotive',
             'country': 'US',
+            'symbol': 'TYT.L'
         },
         {
             'name': 'Mercedes',
             'sector': 'Automotive',
             'country': 'US',
+            'symbol': 'MBG.DE'
         },
         {
             'name': 'Volkswagen',
             'sector': 'Automotive',
             'country': 'US',
+            'symbol': 'VOW3.DE'
+
         },
         {
             'name': 'Ford',
             'sector': 'Automotive',
             'country': 'US',
+            'symbol': 'F'
         },
         {
             'name': 'Bmw',
             'sector': 'Automotive',
             'country': 'US',
+            'symbol': 'BMW.DE'
         }
     ];
 
 const FinanceCompanies =
     [
         {
-            'name': 'Monzo',
+            'name': 'Paypal',
             'sector': 'Finance',
-            'country': 'U.K',
+            'country': 'U.S',
+            'symbol': 'PYPL'
         },
         {
             'name': 'Deutschebank',
             'sector': 'Finance',
             'country': 'Germany',
-        },
-        {
-            'name': 'Starlingbank',
-            'sector': 'Finance',
-            'country': 'U.K',
+            'symbol': 'DBK.DE'
         },
         {
             'name': 'Chase',
             'sector': 'Finance',
             'country': 'US',
+            'symbol': 'JPM'
+        },
+        {
+            'name': 'Visa',
+            'sector': 'Finance',
+            'country': 'U.S',
+            'symbol': 'V'
         },
         {
             'name': 'Barclays',
             'sector': 'Finance',
             'country': 'US',
+            'symbol': 'BCS'
         }
     ];
 
@@ -102,12 +117,10 @@ const ListsContainer = () => {
     return (
         <>
             <StockListContainer style={{ display: 'flex', justifyContent: 'space-evenly', width: '85%', borderRadius: '5px' }}>
-
                 <StockList companies={TechCompanies} sector={'Technology'} />
                 <StockList companies={AutomotiveCompanies} sector={'Automotive'} />
                 <StockList companies={FinanceCompanies} sector={'Finance'} />
             </StockListContainer>
-
         </>
     )
 }
