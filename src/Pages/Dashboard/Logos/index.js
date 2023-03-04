cf97dd65be4cd6a31641179d6837cb8d96bdd449
therealestdeal <mieyle@protonmail.com>
2023-03-04 06:55:19 +0000

    Add all separate file for all logos

---
+++ src/Pages/Dashboard/Logos/index.js
@@ -0,0 +1,89 @@
+
+
+
+import styled from "styled-components";
+import { Apple } from '@styled-icons/boxicons-logos/Apple'
+import { Amazon } from '@styled-icons/boxicons-logos/Amazon'
+import { Ibm } from '@styled-icons/simple-icons/Ibm'
+import { Meta } from '@styled-icons/boxicons-logos/Meta'
+import { Microsoft } from '@styled-icons/boxicons-logos/Microsoft'
+import { Volkswagen } from '@styled-icons/simple-icons/Volkswagen'
+import { Paypal } from '@styled-icons/boxicons-logos/Paypal';
+import { Toyota } from '@styled-icons/simple-icons/Toyota'
+import { Mercedes } from '@styled-icons/simple-icons/Mercedes';
+import { Ford } from '@styled-icons/simple-icons/Ford'
+import { Bmw } from '@styled-icons/simple-icons/Bmw';
+import { Chase } from '@styled-icons/simple-icons/Chase'
+import { Visa } from '@styled-icons/simple-icons/Visa'
+import { Barclays } from '@styled-icons/simple-icons/Barclays'
+import { Deutschebank } from '@styled-icons/simple-icons/Deutschebank';
+
+export const AppleLogo = styled(Apple)`
+  width:50px;
+  color:#000000;
+`
+export const AmazonLogo = styled(Amazon)`
+  width:50px;
+  color:#FF9900;
+`
+export const IbmLogo = styled(Ibm)`
+  width:50px;
+  color:#006699;
+`
+export const MetaLogo = styled(Meta)`
+  width:50px;
+  color:#4267B2;
+`
+export const MicrosoftLogo = styled(Microsoft)`
+  width:50px;
+  color: #008ad7;
+`
+export const MercedesLogo = styled(Mercedes)`
+  width:50px;
+  color:white;
+`
+export const BmwLogo = styled(Bmw)`
+  width:50px;
+  color: #133a7c;
+`
+export const FordLogo = styled(Ford)`
+  width:50px;
+  color:#2a6bac;
+`
+export const VolkswagenLogo = styled(Volkswagen)`
+  width:50px;
+  color:#2a6bac;
+`
+export const ToyotaLogo = styled(Toyota)`
+  width:50px;
+ background-image: linear-gradient(-180deg, rgba(255,255,255,0.50) 0%, rgba(0,0,0,0.50) 100%);
+ background-blend-mode: lighten;
+`
+export const PaypalLogo = styled(Paypal)`
+  width:50px;
+  color:#e83860;
+`
+export const VisaLogo = styled(Visa)`
+  width:50px;
+  color:#6935D3 ;
+`
+export const ChaseLogo = styled(Chase)`
+  width:50px;
+  color:#117ACA ;
+`
+export const BarclaysLogo = styled(Barclays)`
+  width:50px;
+  color:white,
+`
+export const DeutschebankLogo = styled(Deutschebank)`
+  width:50px;
+  color:#0018a8  ;
+`
+export const LogoContainer = styled.div`
+    border-radius:5px;
+    display:flex;
+    height:60px;
+    justify-content:centr;
+    align-items:center;
+    margin:5px;
+`
