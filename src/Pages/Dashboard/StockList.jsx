9e4407bf0aadc3cf80839388cd6fed7f0845c068
therealestdeal <mieyle@protonmail.com>
2023-06-01 17:06:53 +0100

    Change font

--- src/Pages/Dashboard/StockList.jsx
+++ src/Pages/Dashboard/StockList.jsx
@@ -5,7 +5,7 @@ import StockListCard from './StockListCard'
 const StockList = ({ companies, sector }) => {
     return (
         <div style={{ height: '75vh', overflow: 'scroll', display: 'flex', flexDirection: 'column', }} >
-            <h3 style={{ margin: 'auto', fontFamily: 'Ibm Plex Sans', color: '#f7f1e3' }}> {sector}</h3>
+            <h3 style={{ margin: 'auto', fontFamily: 'Roboto', color: '#f7f1e3' }}> {sector}</h3>
             {companies.map(e => (
                 <StockListCard key={e.name} company={e} />
             ))
